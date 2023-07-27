c13b5370b8154bd5f039774fe8e5b15e68afec10
XorySh <melchordiazreyes@gmail.com>
2023-07-27 19:28:47 +0200

    PipesApp creacion de la estructura con primeng, navbar y routing, hechos los pipes de textos y fechas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+      this.primengConfig.ripple = true;
+  }
 }
