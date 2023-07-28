bc3c3cb26ed7971c61e5ea3a0905e01b609330ae
XorySh <melchordiazreyes@gmail.com>
2023-07-28 12:12:31 +0200

    Async Pipe terminado

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -39,5 +40,17 @@ export class UncommonPageComponent {
     age: 25,
     address: 'Chiclana de la Frontera, Cádiz'
   };
+
+  //Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap:', value)),
+  );
+
+  public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout(()=> {
+      resolve( 'Tenemos data en la promesa.' )
+    }, 3500);
+  })
+
 
 }
